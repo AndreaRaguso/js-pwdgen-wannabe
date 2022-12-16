a6eeca2515df02da35b963bd656562d2587dd41d
Andrea Raguso <info@andrearaguso.com>
2022-12-16 14:07:06 +0100

    Esercizio Finito

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let nome = prompt('Digita il tuo nome');
+let cognome = prompt('Digita il tuo cognome');
+let colore = prompt('Digita il tuo colore preferito');
+
+
+document.writeln(nome + cognome + colore + "21");
